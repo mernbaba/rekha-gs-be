c373e86788c1681148c398575efadaa6f0ea2de7
academiaindiana <academiaindiana@outlook.com>
2025-01-09 19:46:34 +0000

    update

--- src/main/java/com/rekha/ecommerce/service/OtpService.java
+++ src/main/java/com/rekha/ecommerce/service/OtpService.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.security.SecureRandom;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -11,8 +13,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.lang3.ObjectUtils;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -34,6 +38,8 @@ public class OtpService {
 
 	@Autowired
 	private OtpRepository otpRepository;
+	
+	private PasswordEncoder passwordEncoder;
 
 	/*@Value("${twilio.account-sid}")
 	private String accountSid;
@@ -54,6 +60,9 @@ public class OtpService {
 
 	@Value("${textlocal.sender.id}")
 	private String senderId;
+	
+	@Value("${textlocal.template.id}")
+	private String templateId;
 
 //	private static final String API_URL = "https://api.textlocal.in/send/";
 
@@ -64,6 +73,7 @@ public class OtpService {
 		return String.valueOf(otp);
 	}
 
+	@Transactional
 	public ResponseObject<?> sendOtp(String phoneNumber) {
 
 		try {
@@ -73,75 +83,133 @@ public class OtpService {
 //			Messages msg = new Messages();
 
 			SecUser secUser = secUserRepository.findByPhoneNumber(phoneNumber);
+			
+			if(!ObjectUtils.isEmpty(secUser)) {
+				// taking random 6 char
+				String OTP = generateOTP();
 
-			// taking random 6 char
-			String OTP = generateOTP();
+				otpEntity.setOtp(OTP);
+				otpEntity.setPhoneNumber(phoneNumber);
+				otpEntity.setOtpGentime(LocalDateTime.now());
+				otpEntity.setOtpExptime(LocalDateTime.now().plusMinutes(10));
+				otpEntity.setOtpStatus("GENERATED");
+				otpEntity.setCreatedby("system");
+				otpEntity.setCreateddate(LocalDateTime.now());
 
-			otpEntity.setOtp(OTP);
-			otpEntity.setPhoneNumber(phoneNumber);
-			otpEntity.setOtpGentime(LocalDateTime.now());
-			otpEntity.setOtpExptime(LocalDateTime.now().plusMinutes(10));
-			otpEntity.setOtpStatus("GENERATED");
-			otpEntity.setCreatedby("system");
-			otpEntity.setCreateddate(LocalDateTime.now());
+				/*if (secUser == null && ObjectUtils.isEmpty(secUser)) {
+					
+					// using this code to send OTP for registration
+					
+					messageBody = "Your OTP is " + OTP
+					+ " . Use this to register your mobile number on Rekha. Valid for 10 minutes.";
+					
+					response = ResponseCode.OTP_SENT_TO_YOUR_MOBILE;
+					} else {
+					// use this to send OTP for other requirement
+				}*/
 
-			/*if (secUser == null && ObjectUtils.isEmpty(secUser)) {
-				
-				// using this code to send OTP for registration
-				
-				messageBody = "Your OTP is " + OTP
-				+ " . Use this to register your mobile number on Rekha. Valid for 10 minutes.";
-				
-				response = ResponseCode.OTP_SENT_TO_YOUR_MOBILE;
-				} else {
-				// use this to send OTP for other requirement
-			}*/
+				String messageBody = "Your OTP is "+OTP+". Use this to set new password. Valid for 10 minutes - Rekha Seeds and Pesticides";
 
-			String messageBody = "Your OTP is" + OTP + ". Use this to set new password. Valid for 10 minutes.";
+				save(otpEntity);
 
-			save(otpEntity);
-
-//			sendSms(phoneNumber, messageBody);
-			String otpResponse = sendSMS("+91" + phoneNumber, messageBody);
-			System.out.println("Response from txt local ::" + otpResponse);
-			return new ResponseObject<>(response);
+//				sendSms(phoneNumber, messageBody);
+//				String otpResponse = sendSMS("91" + phoneNumber, messageBody);
+				String otpResponse = sendSMS(phoneNumber, messageBody);
+				System.out.println("Response from txt local ::" + otpResponse);
+				return ResponseObject.success(response);
+			}else {
+				throw new CloudBaseException(ResponseCode.USER_NOT_FOUND);
+			}
 
 		} catch (Exception e) {
-			throw new CloudBaseException(ResponseCode.ERROR_STORING_DATA);
+			throw e;
 		}
 
 	}
 
+//	public String sendSMS(String phoneNumber, String messageBody) {
+//		try {
+//			// Construct the API request data
+//			String apiKey = "apikey=" + txtApiKey;
+//			String message = "&message=" + messageBody;
+//			String sender = "&sender=" + senderId; // Sender ID
+//			String numbers = "&numbers=" + phoneNumber; // Recipient's phone number
+//			String template = "&template_id=" + templateId;
+//
+//			// Open connection to Textlocal API
+//			HttpURLConnection conn = (HttpURLConnection) new URL("https://api.textlocal.in/send/?").openConnection();
+//			String data = apiKey + numbers + message + sender + template; // Combine all parameters
+//			conn.setDoOutput(true);
+//			conn.setRequestMethod("POST");
+//			conn.setRequestProperty("Content-Length", Integer.toString(data.length()));
+//			conn.getOutputStream().write(data.getBytes("UTF-8"));
+//
+//			// Read response from Textlocal
+//			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+//			StringBuffer responseBuffer = new StringBuffer();
+//			String line;
+//			while ((line = rd.readLine()) != null) {
+//				responseBuffer.append(line);
+//			}
+//			rd.close();
+//
+//			// Return the response from Textlocal API
+//			return responseBuffer.toString();
+//		} catch (Exception e) {
+//			System.out.println("Error SMS: " + e);
+//			return "Error: " + e.getMessage();
+//		}
+//	}
+	
 	public String sendSMS(String phoneNumber, String messageBody) {
+		
+		ResponseCode error = null;
+		
 		try {
 			// Construct the API request data
-			String apiKey = "apikey=" + txtApiKey;
-			String message = "&message=" + messageBody;
-			String sender = "&sender=" + "TXTLCL"; // Sender ID
-			String numbers = "&numbers=" + phoneNumber; // Recipient's phone number
+			String apiKey = "apiKey=" + txtApiKey;
+			String message = "&message=" + URLEncoder.encode(messageBody, "UTF-8");
+			String sender = "&sender=" + senderId; // Sender ID
+			String numbers = "&numbers=" + URLEncoder.encode(phoneNumber, "UTF-8"); // Recipient's phone number
+			
+			String apiUrl = "https://api.textlocal.in/send/?" + apiKey + message + sender + numbers;
+			
+			URL url = new URL(apiUrl);
+			URLConnection conn = url.openConnection();
 
 			// Open connection to Textlocal API
-			HttpURLConnection conn = (HttpURLConnection) new URL("https://api.textlocal.in/send/?").openConnection();
-			String data = apiKey + numbers + message + sender; // Combine all parameters
+//			HttpURLConnection conn = (HttpURLConnection) new URL("https://api.textlocal.in/send/?").openConnection();
+//			String data = apiKey + numbers + message /* + sender */; // Combine all parameters
 			conn.setDoOutput(true);
-			conn.setRequestMethod("POST");
-			conn.setRequestProperty("Content-Length", Integer.toString(data.length()));
-			conn.getOutputStream().write(data.getBytes("UTF-8"));
+//			conn.setRequestMethod("POST");
+//			conn.setRequestProperty("Content-Length", Integer.toString(data.length()));
+//			conn.getOutputStream().write(data.getBytes("UTF-8"));
 
 			// Read response from Textlocal
 			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			StringBuffer responseBuffer = new StringBuffer();
-			String line;
+//			StringBuffer responseBuffer = new StringBuffer();
+			String line = "";
+			StringBuilder sb = new StringBuilder();
 			while ((line = rd.readLine()) != null) {
-				responseBuffer.append(line);
+//				responseBuffer.append(line);
+				sb.append(line).append("\n");
 			}
-			rd.close();
-
-			// Return the response from Textlocal API
-			return responseBuffer.toString();
+			System.err.println(sb.toString());
+//			rd.close();
+			JSONObject jsonResponse = new JSONObject(sb.toString());
+			if(!jsonResponse.optString("status").equalsIgnoreCase("success")) {
+				error = ResponseCode.FAILED_TO_SEND_OTP;
+				throw new CloudBaseException(error);
+			}
+			return sb.toString();
+			
 		} catch (Exception e) {
-			System.out.println("Error SMS: " + e);
-			return "Error: " + e.getMessage();
+			e.printStackTrace();
+			if(error != null) {
+				throw new CloudBaseException(error);
+			}else {
+				throw new CloudBaseException(ResponseCode.UNKNOWN_ERROR_OCCURRED);
+			}
 		}
 	}
 
@@ -242,10 +310,9 @@ public class OtpService {
 				}
 
 				if (otpEntity.getOtp().equalsIgnoreCase(otp)) {
-					secUserRepository.updatePassword(secUser.getPhoneNumber(), pwd, secUser.getUserName());
+					secUserRepository.updatePassword(secUser.getPhoneNumber(), passwordEncoder.encode(pwd), secUser.getUserName());
 					otpRepository.updateOtpStatusByIdAndPartyCode("SUCCESS", otpEntity.getId());
-					throw new CloudBaseException(ResponseCode.SUCCESS);
-
+					return new ResponseObject<>(ResponseCode.SUCCESS);
 				} else {
 					throw new CloudBaseException(ResponseCode.INVALID_OTP);
 				}
